5fe23c0dcac811ae9ee01d98f8351ab555d7bb31
jin woo Lee <sbr08012@naver.com>
2023-12-19 15:38:34 +0900

    백엔드

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
-
+const {auth} = require('./middleware/auth');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const {User} = require("./models/User");
 app.use(express.json()) //For JSON requests
 app.use(express.urlencoded({extended: true}));
-
+app.use(cookieParser());
 const mongoose = require('mongoose')
 mongoose
   .connect(config.mongoURI)
@@ -28,8 +29,57 @@ app.post('/register',(req,res)=>{
           success:true
       })
   }).catch((err)=> res.json({success:false,err}));
+})
+app.post('/login',(req,res)=>{
+  User.findOne({email: req.body.email})
+    .then(docs=>{
+        if(!docs){
+            return res.json({
+                loginSuccess: false,
+                messsage: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+        docs.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({loginSuccess: false, messsage: "비밀번호가 틀렸습니다."})
+    // Password가 일치하다면 토큰 생성
+            docs.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                // 토큰을 저장
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+            })
+        })
+    })
+    .catch((err)=>{
+        return res.status(400).send(err);
+    })
+})
+
+app.get('/auth',auth,(req, res)=>{
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
 
 
+app.get('/api/users/logout', auth,(req, res)=>{
+  User.findOneAndUpdate({_id: req.user._id},{token: ""})
+  .then((user)=>{
+    return res.status(200).send({
+      success: true
+    });
+  })
+  .catch((err)=>{
+    return res.json({success:false,err});
+  })
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
  const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const jwt = require('jsonwebtoken');
  const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -30,7 +32,52 @@
         type: Number
     }
  })
+ userSchema.pre('save', function(next){
+    var user = this;
+if(user.isModified('password')){
+      //비번 암호화
+   bcrypt.genSalt(saltRounds, function (err, salt){
+    if(err) return next(err)
+    bcrypt.hash(user.password,salt,function(err, hash){
+    if(err) return next(err)
+    user.password = hash
+    next()
+    })
+    });
+}else{
+    next();
+}
+})
 
+ userSchema.methods.comparePassword = function(plainPassword, cb){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        return cb(null, isMatch);
+    })
+ }
+ //docs.generateToken this=docs
+ userSchema.methods.generateToken = function(cb){
+   var user = this;
+   var token =jwt.sign(user._id.toHexString(),'secret')
+   user.token = token
+   
+   user.save().then(()=>{
+     cb(null,user)
+   }).catch((err)=>cb(err));
+ }
+ //statics에서 this는 mongoose모델을 의미
+ userSchema.statics.findByToken = function(token, cb){
+    var user = this;
+    jwt.verify(token,'secret',function(err, decoded){
+        user.findOne({"_id":decoded,"token":token})
+        .then((user)=>{
+            return cb(null, user);
+        })
+        .catch((err)=>{
+           return cb(err);
+        });
+    });
+ };
  const User = mongoose.model('User', userSchema)
 
  module.exports = { User }
